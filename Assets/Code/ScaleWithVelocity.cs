5510a6b0948daee63f63d46af1325b4e6c105ac4
DantesGearbox <mads.engberg@hotmail.com>
2017-11-30 21:41:06 +0100

    Fixed collision

--- Assets/Code/ScaleWithVelocity.cs
+++ Assets/Code/ScaleWithVelocity.cs
@@ -6,11 +6,12 @@ public class ScaleWithVelocity : MonoBehaviour {
 
 	Rigidbody2D rb;
 	PlayerLocomotion pl;
+	BoxCollider2D bc;
 
 	private float maxXSpeed;
 	private float maxYSpeed = 0.0f;
 	private float minScaleX = 1.0f;
-	private float maxScaleX = 1.2f;
+	private float maxScaleX = 1.5f;
 	private float minScaleY = 1.0f;
 	private float maxScaleY = 2.0f;
 
@@ -18,22 +19,32 @@ public class ScaleWithVelocity : MonoBehaviour {
 	void Start () {
 		rb = GetComponent<Rigidbody2D> ();
 		pl = GetComponent<PlayerLocomotion> ();
+		bc = GetComponent<BoxCollider2D> ();
+
 		maxXSpeed = pl.GetMaxXSpeed ();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-		//We don't know what the maxJumpVelocity will be so we just update it to be the highest we've seen
-		if(Mathf.Abs(rb.velocity.y) > maxYSpeed){
-			maxYSpeed = Mathf.Abs (rb.velocity.y);
-		}
+		updateMaxYSpeed ();
 
 		//How lerped will the scale be
 		float xScale = Mathf.Abs (rb.velocity.x) / maxXSpeed;
 		float yScale = Mathf.Abs (rb.velocity.y) / maxYSpeed;
-		//Debug.Log ("xScale: " + xScale + ", yScale: " + yScale);
+		float xLerp = Mathf.Lerp (minScaleX, maxScaleX, xScale);
+		float yLerp = Mathf.Lerp (minScaleY, maxScaleY, yScale);
 
-		transform.localScale = new Vector2 (Mathf.Lerp (minScaleX, maxScaleX, xScale), Mathf.Lerp (minScaleY, maxScaleY, yScale));
+		bc.size = new Vector2 (1/xLerp, 1/yLerp);
+
+		//Debug.Log ("xLerp: " + xLerp + ", yLerp: " + yLerp);
+		transform.localScale = new Vector2 (xLerp, yLerp);
+	}
+
+	void updateMaxYSpeed(){
+		//We don't know what the maxJumpVelocity will be so we just update it to be the highest we've seen
+		if(Mathf.Abs(rb.velocity.y) > maxYSpeed){
+			maxYSpeed = Mathf.Abs (rb.velocity.y);
+		}
 	}
 }
